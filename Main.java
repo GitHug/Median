5e28de2232e26bc776504e815e35414f2b56af76
Fredrik Mäkilä <fredrik.makila@gmail.com>
2020-01-11 11:54:56 +0000

    Make time and memory complexity better

--- Main.java
+++ Main.java
@@ -1,34 +1,72 @@
 class Main {
   public static void main(String[] args) {
     System.out.println(new Solution().median(new int[] { 1, 3, 5 }, new int[] { 2, 4, 6} ));
-    System.out.println(new Solution().median(new int[] { 1, 3, 5 }, new int[] { 2, 4, 6, 7} ));
+    System.out.println(new Solution().median(new int[] { 1, 3, 5, 7 }, new int[] { 2, 4, 6, 7} ));
   }
 }
 
 class Solution {
-  double median(int[] arr1, int[] arr2) {
-    int[] arr3 = new int[arr1.length + arr2.length];
 
-    int i = 0, j = 0, k = 0;
+  private static class Subarray {
+    private int[] underlying;
+    private int start;
+    private int size;
 
-    while (i < arr1.length || j < arr2.length) {
-      if (i == arr1.length) {
-        arr3[k++] = arr2[j++];
-      } else if (j == arr2.length) {
-        arr3[k++] = arr1[i++];
-      } else if (arr1[i] < arr2[j]) {
-        arr3[k++] = arr1[i++];
-      } else {
-        arr3[k++] = arr2[j++];
-      }
+    private Subarray(int[] underlying, int start, int size) {
+      this.underlying = underlying;
+      this.start = start;
+      this.size = size;
     }
 
-    if (arr3.length % 2 == 0) {
-      int a = arr3.length / 2;
-      int b = a - 1;
+    private static Subarray fromArray(int[] arr) {
+      return new Subarray(arr, 0, arr.length);
+    }
 
-      return (arr3[a] + arr3[b]) / 2.0;
-    } 
-    return arr3[arr3.length / 2];
+    private Subarray subarray(int start, int end) {
+      if (start > end) throw new IllegalArgumentException();
+      if (end > size) throw new IndexOutOfBoundsException();
+
+      return new Subarray(underlying, this.start + start, end - start);
+    }
+
+    private int getSize() {
+      return size;
+    }
+
+    private int getFirst() {
+      return underlying[start];
+    }
+
+    private int getLast() {
+      return underlying[start + size - 1];
+    }
+
+    private double getMedian() {
+      if (size == 0) return 0;
+
+      if (size % 2 == 0) {
+        return (underlying[start + size / 2 - 1] + underlying[start + size / 2]) / 2.0;
+      }
+      return underlying[start + size / 2];
+    }
+  }
+
+  double median(int[] arr1, int[] arr2) {
+    if (arr1 == null || arr2 == null || arr1.length == 0 || arr1.length != arr2.length) throw new IllegalArgumentException();
+
+    return median(Subarray.fromArray(arr1), Subarray.fromArray(arr2));
+  }
+
+  private double median(Subarray arr1, Subarray arr2) {
+    if (arr1.getSize() == 1) return (arr1.getFirst() + arr2.getFirst()) / 2.0;
+    if (arr1.getSize() == 2) return (Math.max(arr1.getFirst(), arr2.getFirst()) + Math.min(arr1.getLast(), arr2.getLast())) / 2.0;
+
+    double median1 = arr1.getMedian();
+    double median2 = arr2.getMedian();
+
+    if (median1 == median2) return median1;
+    if (median1 > median2) return median(arr1.subarray(0, arr1.getSize() / 2 + 1), arr2.subarray((arr2.getSize() - 1) / 2, arr2.getSize()));
+
+    return median(arr1.subarray((arr1.getSize() - 1) / 2, arr1.getSize()), arr2.subarray(0, arr2.getSize() / 2 + 1)); 
   }
 }
